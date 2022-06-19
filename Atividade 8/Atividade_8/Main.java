9dde66faf77381a5dba3c82d5e04707eda99e80f
Matheus Fialho <matheuslf25@gmail.com>
2022-06-19 16:56:15 -0300

    atividade 8

---
+++ Atividade 8/Atividade_8/Main.java
@@ -0,0 +1,24 @@
+package Atividade_8;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String args[]){
+       Vetor numeros = new Vetor();
+       for(int i =0; i<1000; i++){
+        numeros.adiciona(i);
+       }
+       long tempoInicialRec = System.currentTimeMillis();
+       System.out.println(numeros.buscaBinRec(numeros.numeros, 0, numeros.tamanho()-1, numeros.tamanho()-1));
+       long tempoFinalRec = System.currentTimeMillis();
+
+       long tempoInicialInt = System.currentTimeMillis();
+       System.out.println(numeros.buscaBinIter(numeros.tamanho()-1));
+       long tempoFinalInt = System.currentTimeMillis();
+
+       long tempoRecursão = tempoFinalRec - tempoInicialRec;
+       Long tempoIteração = tempoFinalInt - tempoInicialInt;
+       System.out.println("Tempo de Execução Recursivo: "+ tempoRecursão);
+       System.out.println("Tempo de Execução Iterativo: "+ tempoIteração);
+    }
+}
---
+++ Atividade 8/Atividade_8/Vetor.java
@@ -0,0 +1,66 @@
+package Atividade_8;
+
+import javax.print.event.PrintEvent;
+
+public class Vetor {
+        public int numeros[] = new int[1000];
+        private int totalNum = 0;
+
+        public void adiciona(int num){
+            this.numeros[totalNum] = num;
+            totalNum++;
+        }
+
+        public int tamanho(){
+            return this.totalNum;
+        }
+
+        public int buscaBinIter(int num){
+            int inicio = 0;
+            int fim = this.totalNum;
+            while (inicio<fim){
+                int meio = (inicio+fim)/2;
+                if(this.numeros[meio] == num){
+                    return meio;
+                }
+                else if(this.numeros[meio] > num){
+                    fim = meio - 1;
+                }
+                else{
+                    inicio = meio + 1;
+                }
+            }
+            return -1;
+        }
+
+        public int buscaBinRec(int[] numeros, int inicio, int fim, int num){
+            int meio = (inicio+fim)/2;
+            if(numeros[meio] == num){
+                return numeros[meio];
+            }
+            else if(numeros[meio] > num){
+                int x = buscaBinRec(numeros, inicio, meio-1, num);
+                return x;
+            }
+            else{
+                int x = buscaBinRec(numeros, meio+1, fim, num);
+                return x;
+            }
+        }
+        public int maximoRec(int[] numeros, int inicio, int fim){
+            if(inicio == fim){
+                return numeros[inicio];
+            }
+            else{
+                int meio = (inicio+fim)/2;
+                int max1 = maximoRec(numeros, inicio, meio);
+                int max2 = maximoRec(numeros, meio+1, fim);
+                if(max1 > max2){
+                    return max1;
+                }
+                else{
+                    return max2;
+                }
+            }
+        }
+}
